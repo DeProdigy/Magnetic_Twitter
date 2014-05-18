1e63f191984e5ee6db0cd17fd4928141a5ef6644
DeProdigy <alex.hint.ah@gmail.com>
2014-05-18 15:45:30 -0400

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagneticTwitter::Application.config.secret_token = 'd5cd4d4cbfc036f55c942fbaf0a8cf24558a580b7bb07e197b916f9e232f82b31fc9155851d80663ccfcaab725e40d5ea71a637bcc9eabc7c5f7e5d9e1f5dac0'
